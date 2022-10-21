c88a719b1de24564ed413dfe5ce70841e83bbfb0
smalaca <ab13.krakow@gmail.com>
2022-10-21 19:06:24 +0200

    add getters to ProductReadModel

--- src/main/java/com/smalaca/onlineshop/query/product/ProductReadModel.java
+++ src/main/java/com/smalaca/onlineshop/query/product/ProductReadModel.java
@@ -1,5 +1,7 @@
 package com.smalaca.onlineshop.query.product;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -7,6 +9,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "PRODUCTS")
+@Getter
 public class ProductReadModel {
     @Id
     private UUID id;
